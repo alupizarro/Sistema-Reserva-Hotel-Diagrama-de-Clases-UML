486305e5df6e603f7201f52f094b2375664ec0d4
lucas1 <nahuelluca1203@gmai.com>
2026-05-30 18:20:49 -0300

    Agrega archivos recepcionsta

---
+++ src/Recepcionista/Recepcionista.java
@@ -0,0 +1,81 @@
+public class Recepcionista {
+
+// ATRIBUTOS
+
+private int idRecepcionista;
+private String nombre;
+private String apellido;
+private String turno;
+
+// CONSTRUCTOR
+
+public Recepcionista(int idRecepcionista, String nombre,
+String apellido, String turno) {
+
+this.idRecepcionista = idRecepcionista;
+this.nombre = nombre;
+this.apellido = apellido;
+this.turno = turno;
+}
+
+// GETTERS Y SETTERS
+
+public int getIdRecepcionista() {
+return idRecepcionista;
+}
+
+public void setIdRecepcionista(int idRecepcionista) {
+this.idRecepcionista = idRecepcionista;
+}
+
+public String getNombre() {
+return nombre;
+}
+
+public void setNombre(String nombre) {
+this.nombre = nombre;
+}
+
+public String getApellido() {
+return apellido;
+}
+
+public void setApellido(String apellido) {
+this.apellido = apellido;
+}
+
+public String getTurno() {
+return turno;
+}
+
+public void setTurno(String turno) {
+this.turno = turno;
+}
+
+// METODOS
+
+public void registrarCliente() {
+System.out.println("Cliente registrado correctamente");
+}
+
+public void verReservas() {
+System.out.println("Mostrando listado de reservas");
+}
+
+public void confirmarReserva() {
+System.out.println("Reserva confirmada");
+}
+
+public void gestionarEstadoReserva() {
+System.out.println("Estado de reserva actualizado");
+}
+
+public void registrarCheckIn() {
+System.out.println("Check-in realizado");
+}
+
+public void registrarCheckOut() {
+System.out.println("Check-out realizado");
+}
+
+}
